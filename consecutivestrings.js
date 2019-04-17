f2e50a08260c78bfc6e9a87892ace786ced7cb62
Justin Gustafson <jtwgus@gmail.com>
2019-04-17 10:36:44 -0400

    Completed COdewars challenge

---
+++ consecutivestrings.js
@@ -0,0 +1,17 @@
+// You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+
+
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
+
+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2);
